d5788fd2a8b0db7f9189b322597a80c57bc513df
SeulYoung <38755691+SeulYoung@users.noreply.github.com>
2021-01-17 13:30:56 +0800

    调整文件结构

--- easy/add_two_numbers.py
+++ easy/add_two_numbers.py
@@ -5,21 +5,20 @@ class ListNode:
         self.next = next
 
 
-class Solution:
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        head = curr = ListNode()
-        carry = 0
+def addTwoNumbers(l1: ListNode, l2: ListNode) -> ListNode:
+    head = curr = ListNode()
+    carry = 0
 
-        while l1 or l2 or carry:
-            val = carry
-            if l1:
-                val += l1.val
-                l1 = l1.next
-            if l2:
-                val += l2.val
-                l2 = l2.next
-            carry, val = divmod(val, 10)
-            curr.next = curr = ListNode(val)
-            curr = curr.next
+    while l1 or l2 or carry:
+        val = carry
+        if l1:
+            val += l1.val
+            l1 = l1.next
+        if l2:
+            val += l2.val
+            l2 = l2.next
+        carry, val = divmod(val, 10)
+        curr.next = curr = ListNode(val)
+        curr = curr.next
 
-        return head.next
+    return head.next
--- easy/reverse.py
+++ easy/reverse.py
@@ -1,20 +1,20 @@
-class Solution:
-    def reverse(self, x: int) -> int:
-        str_x = str(x)
-        if str_x[0] == '-':
-            x = - int(str_x[:0:-1])
-        else:
-            x = int(str_x[::-1])
-        return x if -2147483648 < x < 2147483647 else 0
+def reverse(x: int) -> int:
+    str_x = str(x)
+    if str_x[0] == '-':
+        x = - int(str_x[:0:-1])
+    else:
+        x = int(str_x[::-1])
+    return x if -2147483648 < x < 2147483647 else 0
 
-    def reverse(self, x: int) -> int:
-        abs_x, rev = abs(x), 0
-        # boundary = 2147483647 if x > 0 else 2147483648
-        boundary = (1 << 31) - 1 if x > 0 else 1 << 31
-        
-        while abs_x != 0:
-            rev = rev * 10 + abs_x % 10
-            if rev > boundary:
-                return 0
-            abs_x //= 10
-        return rev if x > 0 else -rev
+
+def reverse(x: int) -> int:
+    abs_x, rev = abs(x), 0
+    # boundary = 2147483647 if x > 0 else 2147483648
+    boundary = (1 << 31) - 1 if x > 0 else 1 << 31
+
+    while abs_x != 0:
+        rev = rev * 10 + abs_x % 10
+        if rev > boundary:
+            return 0
+        abs_x //= 10
+    return rev if x > 0 else -rev
