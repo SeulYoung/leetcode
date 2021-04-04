2d0d74e52a43763356cd90dc7220c76d6e0d7981
SeulYoung <38755691+SeulYoung@users.noreply.github.com>
2021-04-04 15:38:12 +0800

    三步问题；圆圈最后所剩数字

---
+++ easy/ways_to_step.py
@@ -0,0 +1,31 @@
+class Solution:
+    def __init__(self):
+        self.count = 0
+
+    def waysToStep(self, n: int) -> int:
+        def dfs(n):
+            if n == 1:
+                self.count += 1
+            elif n == 2:
+                self.count += 2
+            elif n == 3:
+                self.count += 4
+            else:
+                dfs(n - 1)
+                dfs(n - 2)
+                dfs(n - 3)
+
+        dfs(n)
+        return self.count
+
+    def waysToStep(self, n: int) -> int:
+        if n < 3:
+            return n
+        elif n == 3:
+            return 4
+
+        dp0, dp1, dp2 = 1, 2, 4
+        for i in range(4, n + 1):
+            dp0, dp1, dp2 = dp1, dp2, (dp0 + dp1 + dp2) % 1000000007
+
+        return dp2
