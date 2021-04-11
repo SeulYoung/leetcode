2010f488286a96585981e2f98b7055806085004f
SeulYoung <38755691+SeulYoung@users.noreply.github.com>
2021-04-11 20:24:49 +0800

    两数之和

--- easy/two_sum.py
+++ easy/two_sum.py
@@ -1,20 +1,24 @@
 from typing import List
 
 
-def twoSum(nums: List[int], target: int) -> List[int]:
-    n = len(nums)
-    for i in range(n):
-        for j in range(i + 1, n):
-            if nums[i] + nums[j] == target:
-                return [i, j]
-    return []
+#
+# @param numbers int整型一维数组
+# @param target int整型
+# @return int整型一维数组
+#
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        for i in range(len(numbers)):
+            for j in range(i + 1, len(numbers)):
+                if numbers[i] + numbers[j] == target:
+                    return [i + 1, j + 1]
+        return []
 
-
-def twoSum(nums: List[int], target: int) -> List[int]:
-    hashtable = {}
-    for i, num in enumerate(nums):
-        key = target - num
-        if key in hashtable:
-            return [hashtable[key], i]
-        hashtable[num] = i
-    return []
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        hash_map = {}
+        for i, num in enumerate(numbers):
+            key = target - num
+            if key in hash_map:
+                return [hash_map[key] + 1, i + 1]
+            hash_map[num] = i
+        return []
